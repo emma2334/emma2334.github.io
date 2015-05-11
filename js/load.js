980007aa96692d8dd3ec7266d0192d8ca5f94074
Emma <emma2334@gmail.com>
2015-05-11 18:06:46 +0800

    fix timeline bug

--- js/load.js
+++ js/load.js
@@ -1,13 +1,13 @@
 $.get('./timeline.html', function(data){
   $('#timeline').append(data);
-  setTimeout("$.getScript('./js/jquery.timeline.min.js');", 1000);
-  setTimeout("$.getScript('./js/jquery.mCustomScrollbar.js');", 1500);
-  setTimeout("$.getScript('./js/jquery.easing.1.3.js');", 1500);
-  setTimeout("$.getScript('./js/image.js');", 1500);
-  setTimeout("$.getScript('./js/lightbox.js');", 1500);
-  setTimeout("$.getScript('./js/timeline.js');", 2000);
+  $.getScript('./js/jquery.timeline.min.js');
+  $.getScript('./js/jquery.mCustomScrollbar.js');
+  $.getScript('./js/jquery.easing.1.3.js');
+  $.getScript('./js/image.js');
+  $.getScript('./js/lightbox.js');
   $.get('./timeline_extend.html', function(data){
     $('#timeline').append(data);
+    $.getScript('./js/timeline.js');
   });
 });
 
