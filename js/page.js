09ef45dd16a513b067eee7ad0eb312b68fe5d4ed
Emma <emma2334@gmail.com>
2015-08-14 00:09:35 +0800

    integrate some js and css files

--- js/page.js
+++ js/page.js
@@ -1,11 +1,60 @@
-(function(){
-  var $window,fullPage;
-  $window=$(window);
-  fullPage=$(".full-page");
-  fullPage.height($window.height());
-  $(window).on("resize",function(e){return fullPage.height($window.height());});
-}).call(this);
+/*---------------------------
+  Preload
+ ---------------------------*/
+$.get('./img/DSC00063.JPG', function(data){
+  $('#preload').delay(2000).fadeOut(500, function(){
+    $(".parallax_front").fadeIn();
+    $(".parallax_base").fadeIn();
+  });
+}).fail(function(){
+  var msg = $(document.createElement('div')).css('display', 'none').attr('id', 'preload_error').html('Something wrong.<br>Please refresh the page');
+  $('#preload').append(msg);
+  $("#preload_error").fadeIn(500);
+});
 
+
+
+/*---------------------------
+  Transition
+ ---------------------------*/
+(function(){
+  $( window ).on('scroll', function() {
+    progress = parseFloat($(document).scrollTop());
+    var e = progress/$(window).height();
+
+    var a=$('#scroll-down-notice'), b=$('#slogan'), c=0.2-e;
+    if(c>0){
+      a.css("opacity", 4*c);
+      b.css("opacity", 3*c);
+    }else{
+      a.css("opacity", 0);
+      b.css("opacity", 0);
+    }
+
+
+    if(progress*2.5+10<($('.parallax_front').height()-$(window).height())) $('.parallax_front').css('top', -progress*1.5);
+
+    if(progress>0){
+      $('#quote').removeClass("transparent");
+      $('#about').removeClass("transparent");
+    }
+
+    if(($('#skills').offset().top-progress)<$(window).height()/2.5){
+      for(var i=0; i<$('.score').length; i++){
+        var level = $('.score').eq(i).attr('data-score');
+        $('.score').eq(i).addClass('score-'+level);
+      }
+    }
+  });
+
+})();
+
+
+
+
+/*---------------------------
+  Skill table
+ ---------------------------*/
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
   for(var i=0; i<$('.score').length; i++){
@@ -20,15 +69,60 @@ $(function () {
   }
 });
 
+
+
+/*---------------------------
+  Alert
+ ---------------------------*/
 function github_alert(){
-      alert("It's a private repository.");
-    }
+  alert("It's a private repository.");
+}
 function link_alert(){
   alert("The link is empty.");
 }
 
-$(function() {
-  $("img.lazy").lazyload({
-    // effect : "fadeIn"
+
+
+/*---------------------------
+  Loading
+ ---------------------------*/
+$(function(){
+  var flag = 0;
+  $( window ).on('scroll', function() {
+    if(($('#timeline').offset().top-progress+50)<$(window).height() && flag==0){
+      flag=1;
+      $.get('./_timeline.html', function(data){
+        $('#timeline').append(data);
+        $.getScript('./js/jquery.timeline.min.js');
+        $.getScript('./js/jquery.mCustomScrollbar.js');
+        $.getScript('./js/jquery.easing.1.3.js');
+        $.getScript('./js/image.js');
+        $.getScript('./js/lightbox.js');
+        $.get('./_timeline_extend.html', function(extend){
+          $('#timeline').append(extend);
+          $.getScript('./js/timeline.js');
+        });
+      });
+    }
+
+    if(($('#activity').offset().top-progress+50)<$(window).height() && flag==1){
+      flag=2;
+      $.get('./_activities.html', function(data){
+        $('#activity').append(data);
+      });
+    }
+  });
+});
+
+$.get('./_works.html', function(data){
+  $('#works .container').append(data);
+  $(function(){
+    $('figure.effect-zoe').hover(
+      function(){
+        $(this).find('.mask').css('opacity', '0.3');
+      },
+      function(){
+        $(this).find('.mask').css('opacity', '');
+      });
   });
 });
--- js/preloader.js
+++
@@ -1,6 +0,0 @@
-$.get('./img/DSC00063.JPG', function(data){
-  $('#preload').delay(2000).fadeOut(500, function(){
-    $(".parallax_front").fadeIn();
-    $(".parallax_base").fadeIn();
-  });
-});
