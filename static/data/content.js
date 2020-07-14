8078cd3c5593e7354e83e77d695b73bf21f9c71d
Emma <emma2334@gmail.com>
2020-07-14 15:08:20 +0800

    feat: complete chinese content
    - modify layout
    - normalize css and use bootstrap grid system
    - remove redundancy

--- static/data/content.js
+++ static/data/content.js
@@ -1,48 +1,129 @@
 var CONTENT = {
   'zh-tw': {
     nav: {
-      about: '關於我',
+      sections: {
+        about: '關於我',
+        skill: '技能',
+        experience: '工作經驗',
+        portfolio:'作品集',
+      }
     },
     about: {
-      title: '關於我',
       info: [
         '嗨，我是 Emma，目前致力於前端開發，也懂得少許後端技術。',
         '曾經以交換學生的身份到歐洲待了半年，閒暇之餘也走過歐洲許多城市。樂於擁抱新事物，因此交換期間內並沒有遇到太大的文化衝擊，反倒嘗試了很多新事物，例如：滑雪、跳傘、一個人的長途旅行、couchsurfing、訂了沒有共同語言的外國人的共乘（當時我透過 google translate 和他溝通，結局是一段非常美好的回憶）。',
         '大學時期在學長的介紹下第一次參加了資訊研討會—— 2013 年的 COSCUP，受到社群與開源精神的感召，至今仍以與會者或志工的身份穿梭大小研討會間。雖然技術還不甚純熟，但認為學習新知識是一件愉悅的事情，正在以自己的步調成長中。除了技術外，對人文和藝術領域也相當感興趣。',
       ],
-      details: [
+    },
+    skill: {
+      skills: {
+        frontend: {
+          CSS: 4,
+          JavaScript: 3,
+          React: 2,
+        },
+        backend: {
+          'Node.js': 2,
+          Rails: 1,
+          PHP: 1,
+        },
+        others: {
+          MongoDB: 2,
+          'Meteor.js': 2,
+        },
+        tools: {
+          Git: 3,
+        }
+      }
+    },
+    experience: {
+      list: [
         {
-          icon: 'fa fa-graduation-cap',
-          text: '國立中興大學<span>2012-2016</span><br>歐波蘭大學<span>2016</span>'
+          company: '中興⼤學圖書館校史館組',
+          title: '工讀生',
+          duration: '2015/1 - 2016/1',
+          tech: ['jsp', 'Dspace'],
+          info: [
+            '將機構典藏系統前端樣式客製化並串接論⽂分析相關 API（例如 PubMed、Altmetric）。',
+            '偶爾修復系統（JSP 專案）沒有正常運作的功能。',
+          ],
         },
         {
-          icon: 'fa fa-briefcase',
-          text: 'Sharelike 實習生<span>2015/7-2015/8</span><br>中興大學圖書館校史館組工讀生<span>2014/9-2016/1</span>'
+          company: '享萊 Sharelike',
+          title: '實習生',
+          duration: '2015/7 - 2015/8',
+          tech: ['Rails', 'WordPress'],
+          info: [
+            '期間內接受 Ruby on Rails 訓練，開發通⽤點數功能。',
+            '與⾏銷部⾨配合改善公司形象網⾴，導入 SEO 加強網⾴曝光率，並將預約體驗表格導入 Google 表單以便⾏銷部⾨彙整。',
+          ],
         },
         {
-          icon: 'fa fa-heart',
-          text: '看電影、聽音樂、攝影'
+          company: '數巨飛船',
+          title: '前端工程師',
+          duration: '2017/5 - 2018/10',
+          tech: ['Meteor.js', 'Phaser.js', 'LESS', 'MongoDB', 'WordPress'],
+          info: [
+            '主要負責網⾴前端開發，偶爾兼顧後端開發。',
+            '選定 Meteor 搭配 LESS 和 Bootstrap 為主要開發架構，但仍會依案件類型採⽤不同的框架或後端語⾔。',
+            '⼤多開發活動互動式體驗應⽤，因此⼗分注重⽤⼾體驗和響應式設計，曾經參與麥當勞員⼯⼤會、Samsung S9 點食晨⾦等活動系統開發。',
+          ],
+        },
+        {
+          company: '數果數位',
+          title: '全端工程師',
+          duration: '2018/11 - 2020/7',
+          tech: ['ejs', 'React', 'SASS', 'Node,js', 'MongoDB'],
+          info: [
+            '負責波波黛莉網頁前端與 api 開發，想辦法在瀏覽器與 web app 間取得用戶最佳體驗',
+          ],
+        },
+      ],
+    },
+    portfolio: {
+      list: [
+        {
+          title: '台灣登革熱地圖',
+          type: '靜態網站',
+          img: 'dengue_tw.png',
+          des: '利用 D3.js 將資料視覺化，以判斷各地區登革熱嚴重性（demo 頁資料僅更新至 2018/6）',
+          tags: ['D3.js'],
+          link: 'https://emma2334.github.io/dengue_tw/',
+          github: 'https://github.com/emma2334/dengue_tw',
+        },
+        {
+          title: '導回分類最新文章',
+          type: 'WordPress 插件',
+          img: 'RCLP.png',
+          des: '在設定 WordPress 導覽列時新增文章分類只需點擊勾選就可以設定是否導到該分類最新文章',
+          tags: ['wordpress'],
+          link: 'https://wordpress.org/plugins/redirect-to-latest-post-in-category/',
+          github: 'https://github.com/emma2334/wp-redirect-to-category-latest-post',
+        },
+        {
+          title: '動漫屋閱讀輔助器',
+          type: 'js 腳本',
+          img: 'dm5-reader.png',
+          des: '大學時為了看漫畫寫的腳本，把一些在手機漫畫 app 的優點整併進腳本（內容展開、自動滾動、自動換章）',
+          tags: ['javascript'],
+          link: 'https://emma2334.github.io/DM5-Viewer/',
+          github: 'https://github.com/emma2334/DM5-Viewer',
         }
       ]
     },
-    works: {
-      name: '',
-      img: '',
-    }
   },
   'en': {
     nav: {
-      about: 'about me',
+      sections: {
+        about: 'about me',
+      }
     },
     about: {
-      title: 'about me',
       info: [
         'Hi, I\'m Emma, a full stack web developer, mostly working on frontend development.',
         'I did an exchange program to Poland in 2016 for a half year. I\'m always confident with embracing new things, hence there were more priceless experiences than culture shock. Many first times happened in this duration: skiing, sky diving, first solo long distance trip, couch surfing... and the most precious of all, taking a blah blah car from someone who shared no common language with me. Thanks to Google translation, it turned out to be an extremely wonderful memory',
         'learning new things, it won\'t be a tough process. I\'m eager to be challenged and improve! Excepting IT field, I also find interest in cultures and arts.'
       ],
-    }
+    },
   }
-};
-
-// <a class="btn btn-default" href="https://emma84811.wordpress.com" target="_blank">學習筆記</a>
+}
--- static/js/index.js
+++ static/js/index.js
@@ -1,122 +1,4 @@
 (function() {
-  var isPhone = ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) );
-
-  if(isPhone) $('body').addClass('phone');
-  // Scroll effect
-  // detect if IE : from http://stackoverflow.com/a/16657946
-  var ie = /*@cc_on!@*/false || !!document.documentMode;
-
-  // disable/enable scroll (mousewheel and keys)
-  function preventDefault(e) {
-    e = e || window.event;
-    if (e.preventDefault)
-    e.preventDefault();
-    e.returnValue = false;
-  }
-
-  function disable_scroll() {
-    // window.onmousewheel = document.onmousewheel = function wheel(e) {
-    //   // for IE
-    //   //if( ie ) {
-    //     //preventDefault(e);
-    //   //}
-    // };
-
-    document.onkeydown = function(e) {
-      if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1){
-        preventDefault(e);
-      }
-    };
-
-    document.body.ontouchmove = function(e) {
-      if(isPhone) preventDefault(e);
-    };
-  }
-
-  function enable_scroll() {
-    window.onmousewheel = document.onmousewheel = document.onkeydown = document.body.ontouchmove = null;
-  }
-
-  var docElem = window.document.documentElement,
-    scrollVal,
-    isRevealed,
-    noscroll,
-    isAnimating,
-    container = document.body,
-    trigger = container.querySelector( '#scroll-down-notice' );
-
-  function scrollY() {
-    return window.pageYOffset || docElem.scrollTop;
-  }
-
-  function scrollPage() {
-    scrollVal = scrollY();
-
-    if( noscroll && !ie ) {
-      if( scrollVal < 0 ) return false;
-      // keep it that way
-      window.scrollTo( 0, 0 );
-    }
-
-    if( $('body').hasClass( 'notrans' ) ) {
-      $('body').removeClass('notrans' );
-      return false;
-    }
-
-    if( isAnimating ) {
-      return false;
-    }
-
-    if( scrollVal <= 0 && isRevealed ) {
-      toggle(0);
-    }
-    else if( scrollVal > 0 && !isRevealed && !isPhone ){
-      toggle(1);
-    }
-  }
-
-  function toggle( reveal ) {
-    isAnimating = true;
-
-    if( reveal ) {
-      $('body').addClass( 'modify' );
-    }
-    else {
-      noscroll = true;
-      disable_scroll();
-      $('body').removeClass( 'modify' );
-      $('#home').removeClass('active');
-      lineMaker.hideLines();
-      setTimeout(homeAnimation(), 1200);
-    }
-
-    // simulating the end of the transition:
-    setTimeout( function() {
-      isRevealed = !isRevealed;
-      isAnimating = false;
-      if( reveal ) {
-        noscroll = false;
-        enable_scroll();
-      }
-    }, 1200 );
-  }
-
-  // refreshing the page...
-  var pageScroll = scrollY();
-  noscroll = pageScroll === 0;
-
-  disable_scroll();
-
-  if( pageScroll ) {
-    isRevealed = true;
-    $('body').addClass( 'notrans' );
-    $('body').addClass( 'modify' );
-  }
-
-  window.addEventListener( 'scroll', scrollPage, { passive: false } );
-  trigger.addEventListener( 'click', function() { toggle( 'reveal' ); } );
-
-  homeAnimation();
 
   // Smooth scroll
   $(document).on('click', 'a[href^="#"]', function (event) {
@@ -124,69 +6,48 @@
 
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top + 1
-    }, 500);
-  });
-})();
-
-(function(){
-  window.sr = ScrollReveal({ reset: true });
-
-  sr.reveal($('#about .info'), {
-    origin: 'right',
-    scale: 1,
-    distance: $('.container').css('margin-right'),
-    duration: 2000
+    }, 500)
   });
 
-  // sr.reveal($('#about .row')[1], {
-  //   origin: 'right',
-  //   scale: 1,
-  //   distance: $('.container').css('margin-right'),
-  //   duration: 2000
-  // });
+  // intersection observer
+  new IntersectionObserver(function (entries) {
+    var intersectionRatio = entries[0].intersectionRatio
+    $('#home').toggleClass('active', intersectionRatio >= 0.2)
+    $('nav').toggleClass('active', intersectionRatio != 1)
+  }, {
+    threshold: [...new Array(11)].map((e, i) => i * 0.1),
+  }).observe($('#home')[0])
+
+  var observe = new Map()
+  var sectionObserve = new IntersectionObserver(function (entries, e) {
+    entries.forEach(function (entry) {
+      var target = entry.target
+      var intersectionRatio = entry.intersectionRatio
+      var isIntersecting = entry.isIntersecting
+
+      observe.set(target, isIntersecting)
+
+      if(intersectionRatio >= 0.2) $(target).addClass('active')
+      if(intersectionRatio < 0.1 || !isIntersecting) $(target).removeClass('active')
+    })
+    var sections = $('.content > section').toArray().map(function (e) { return $(e).attr('id')})
+    var highlighted = Array.from(observe.values()).indexOf(true)
+    if (highlighted > -1) {
+        $('nav a').removeClass('active')
+        $('[href=#' + sections[highlighted] + ']').addClass('active')
+    }
+  }, {
+    threshold: [...new Array(11)].map((e, i) => i * 0.1),
+  })
+  $('.content > section').each((i, dom) => {
+    sectionObserve.observe(dom)
+    observe.set(dom, false)
+  })
+
+  $(window).on('load', function() {
+    setTimeout(function() {
+      $('#preload').fadeOut('slow')
+      $('#home').addClass('active')
+    }, 100);
+  });
 })();
-
-var lineMaker = new LineMaker({
-  parent: {element: '#home', position: 'prepend'},
-  lines: [
-    {top: '15%', left: 0, width: '100%', height: 2, color: '#FFF', hidden: true},
-    {top: '85%', left: 0, width: '100%', height: 2, color: '#FFF', hidden: true},
-    {top: 0, left: '90%', width: 2, height: '100%', color: '#FFF', hidden: true},
-    {top: 0, left: '10%', width: 2, height: '100%', color: '#FFF', hidden: true},
-    {top: '15%', left: 0, width: '100%', height: 2, color: '#888', hidden: true, animation: { duration: 1000, easing: 'easeInOutExpo', delay: 0, direction: 'LeftRight' }},
-    {top: '85%', left: 0, width: '100%', height: 2, color: '#888', hidden: true, animation: { duration: 1000, easing: 'easeInOutExpo', delay: 100, direction: 'RightLeft' }},
-    {top: 0, left: '90%', width: 2, height: '100%', color: '#888', hidden: true, animation: { duration: 1000, easing: 'easeInOutExpo', delay: 200, direction: 'BottomTop' }},
-    {top: 0, left: '10%', width: 2, height: '100%', color: '#888', hidden: true, animation: { duration: 1000, easing: 'easeInOutExpo', delay: 300, direction: 'TopBottom' }},
-    {top: '50vh', left: '21vw', width: '60vw', height: 16, color: '#508bf1', hidden: true, animation: { duration: 1000, easing: 'easeInOutExpo', delay: 300, direction: 'LeftRight' }}
-  ]
-});
-
-function homeAnimation(){
-  // setTimeout(function() { lineMaker.hideLines();}, 0);
-  setTimeout(function() {
-    $('#home').addClass('active');
-
-    lineMaker.animateLineIn(4, {
-      complete: function() { lineMaker.showLine(0); }
-    });
-    lineMaker.animateLineIn(5, {
-      complete: function() { lineMaker.showLine(1); }
-    });
-    lineMaker.animateLineIn(6, {
-      complete: function() { lineMaker.showLine(2); }
-    });
-    lineMaker.animateLineIn(7, {
-      complete: function() { lineMaker.showLine(3); }
-    });
-  }, 100);
-
-  setTimeout(function() {
-    lineMaker.animateLineOut(4);
-    lineMaker.animateLineOut(5);
-    lineMaker.animateLineOut(6);
-    lineMaker.animateLineOut(7);
-    lineMaker.animateLineIn(8);
-    $('#scroll-down-notice').delay(1000).fadeIn();
-  }, 1700);
-
-}
